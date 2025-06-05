e8e2e107c8d9a76d21802066888fa1d8c65923f6
Pushubhosle2211 <pushubhosle2211@gmail.com>
2025-06-05 09:46:01 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -88,7 +88,7 @@ LOG_GROUP_ID = getenv("LOG_GROUP_ID", "").strip()
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "6815918609").split())
+    map(int, getenv("OWNER_ID", "7548822418").split())
 )  # Input type must be interger
 
 # Get it from http://dashboard.heroku.com/account
@@ -114,10 +114,10 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/TheTeamVivek"
+    "SUPPORT_CHANNEL", "https://t.me/BOT_OWNER_OP"
 )  # Example:- https://t.me/TheTeamVivek
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/TheTeamVk"
+    "SUPPORT_GROUP", "https://t.me/BOT_OWNER_OP"
 )  # Example:- https://t.me/TheTeamVk
 
 
@@ -142,7 +142,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/TheTeamVivek/YukkiMusic")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://t.me/BOT_OWNER_OP")
 
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
